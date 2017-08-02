ae6935a1126c70ab345dd839fc3ec2e75a5b862e
egoodwintx <ed.registration@gmail.com>
2017-08-01 19:46:21 -0500

    skeleton script for clustering exercise

---
+++ hrugcluster.r
@@ -0,0 +1,14 @@
+##----------------------------------------------------------------
+## Title: hrugcluster.r
+## Author: Ed Goodwin
+## Date: 08.01.2017
+## Description: 
+##  HRUG Intro to Clustering Meetup code
+##----------------------------------------------------------------
+
+# hrugcluster.r BEGIN CODE
+library(ggplot2)
+library(GMD)
+
+hrugclust.dat = 
+  read.csv("https://raw.githubusercontent.com/HoustonUseRs/clusterworkshop/master/hrugcluster.csv")
