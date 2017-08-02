b61ea13474cbab346d7140ed378785d9892d4c11
egoodwintx <ed.registration@gmail.com>
2017-08-01 20:40:38 -0500

    added code to do kmeans clustering

--- hrugcluster.r
+++ hrugcluster.r
@@ -8,7 +8,23 @@
 
 # hrugcluster.r BEGIN CODE
 library(ggplot2)
+library(dplyr)
 library(GMD)
+
+x_clust = 3
 
 hrugclust.dat = 
   read.csv("https://raw.githubusercontent.com/HoustonUseRs/clusterworkshop/master/hrugcluster.csv")
+
+hrugclust.dat = hrugclust.dat[,2:3]
+kmhrug = kmeans(hrugclust.dat, x_clust)
+kmhrug
+kmeansIC(hmhrug)
+
+## Visualize the data
+x = hrugclust.dat %>% cbind(kmhrug$cluster) %>% as.data.frame()
+
+p = ggplot(data = x, aes(x = x[,1], y = x[,2])) + 
+  geom_point(colour = x[,3], size = 4) + 
+  ylab("Distance") + xlab("Pets")
+p
